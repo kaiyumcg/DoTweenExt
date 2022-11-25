98bf47cf0c10fe5e2dd2114aec7b0bed7ae04784
kaiyumPC <kaiyumcg@gmail.com>
2022-11-25 21:44:09 +0600

    unityext purified

---
+++ Runtime/DTCoreEx.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+
+namespace DTExt
+{
+    public static class DTCoreEx
+    {
+        public static void ExResetDT(this Tween dt)
+        {
+            if (dt != null && dt.IsActive()) { dt.Kill(); }
+        }
+        public static bool ExIsValidDT(this Tween dt)
+        {
+            return dt != null && dt.IsActive();
+        }
+        public static void ExResetDT(this List<Tween> dtList)
+        {
+            dtList.ExForEach((dt) =>
+            {
+                if (dt != null) { dt.ExResetDT(); }
+            });
+        }
+    }
+}
